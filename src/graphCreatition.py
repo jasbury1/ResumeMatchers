fedf766c7ab02d5802f1ddc403a4e0d72d10498c
joshuaboe <jboe@calpoly.edu>
2021-02-16 11:18:28 -0800

    Added appending to graphs (#5)
    * changed FILENAME to FILEPATH
    
    * added comments
    
    * added appending to graphs

--- src/graphCreatition.py
+++ src/graphCreatition.py
@@ -14,6 +14,7 @@ def main():
     write_graph(G, FILEPATH)
 
 
+# Create a test knowledge graph
 def generate_test_graph1():
     edge_dict = {}
     edge_dict['JavaScript'] = ['Job1', 'Job2', 'Job3', 'Job4', 'Job5']
@@ -23,14 +24,26 @@ def generate_test_graph1():
     return create_graph(edge_dict)
 
 
-# Create a Knowledge Graph
+# Create a Knowledge Graph, provided a dictionary of nodes and edges
 #   - e_dict: a dictionary of skill-job (key-value) pairs
 def create_graph(e_dict):
     return nx.from_dict_of_lists(e_dict, create_using=nx.DiGraph())
 
 
+# Append new nodes and edges to a Knowledge Graph
+#   - G: a networkx DiGraph
+#   - e_dict: a dictionary of skill-job (key-value) pairs to be added
+def append_graph(G, e_dict):
+    G.add_nodes_from(e_dict)
+    node_edge_pairs = []
+    for skill, jobs in e_dict.items():
+        for job in jobs:
+            node_edge_pairs.append((skill, job))
+    G.add_edges_from(node_edge_pairs)
+
+
 # Visualize a Knowledge Graph
-#   - G: a networkx MultiDiGraph
+#   - G: a networkx DiGraph
 def draw_graph(G):
     sources = []
     for e in G.edges():
@@ -44,10 +57,12 @@ def draw_graph(G):
     plt.show()
 
 
+# Read and return a Knowledge Graph from a file path
 def read_graph(path):
     return nx.read_edgelist(path, create_using=nx.DiGraph, nodetype=str)
 
 
+# Write a Knowledge Graph to a file path
 def write_graph(G, path):
     nx.write_edgelist(G, path)
 
