8d3224e9f59a9b2e0384b0a8efa6a14f094ef077
Joshua Boe <jboe@calpoly.edu>
2021-02-16 09:40:29 -0800

    added comments

--- src/graphCreatition.py
+++ src/graphCreatition.py
@@ -14,6 +14,7 @@ def main():
     write_graph(G, FILEPATH)
 
 
+# Create a test knowledge graph
 def generate_test_graph1():
     edge_dict = {}
     edge_dict['JavaScript'] = ['Job1', 'Job2', 'Job3', 'Job4', 'Job5']
@@ -23,7 +24,7 @@ def generate_test_graph1():
     return create_graph(edge_dict)
 
 
-# Create a Knowledge Graph
+# Create a Knowledge Graph, provided a dictionary of nodes and edges
 #   - e_dict: a dictionary of skill-job (key-value) pairs
 def create_graph(e_dict):
     return nx.from_dict_of_lists(e_dict, create_using=nx.DiGraph())
@@ -44,10 +45,12 @@ def draw_graph(G):
     plt.show()
 
 
+# Read and return a Knowledge Graph from a file path
 def read_graph(path):
     return nx.read_edgelist(path, create_using=nx.DiGraph, nodetype=str)
 
 
+# Write a Knowledge Graph to a file path
 def write_graph(G, path):
     nx.write_edgelist(G, path)
 
