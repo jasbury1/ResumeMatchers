e09ce74c6ec6f908d57c5a96487112929e76e6e9
lacraft <lacraft@calpoly.edu>
2021-02-15 18:01:54 -0800

    changed the parser to use indeed and added some description

--- src/jobParse.py
+++ src/jobParse.py
@@ -1,21 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://www.monster.com/jobs/search/?q=Software-Developer&where=California'
+URL = 'https://www.indeed.com/jobs?q=software+engineer&l=California'
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
-results = soup.find(id='ResultsContainer')
-jobs = results.find_all('section', class_='card-content')
+results = soup.find(id='resultsCol')
+jobs = results.find_all(class_='jobsearch-SerpJobCard')
 
 for job in jobs:
     title_info = job.find('h2', class_='title')
-    company_info = job.find('div', class_='company')
-    location_info = job.find('div', class_='location')
-    if None in (title_info, company_info, location_info):
+    company_info = job.find('span', class_='company')
+    location_info = job.find('span', class_='location')
+    description_info = job.find('div', class_='summary')
+    if None in (title_info, company_info, location_info, description_info):
         continue
     print(title_info.text.strip())
     print(company_info.text.strip())
     print(location_info.text.strip())
+    print(description_info.text.strip())
     print()
