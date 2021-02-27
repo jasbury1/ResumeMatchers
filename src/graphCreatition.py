c39422c662b73beaea4a59104d8b50f2d6ff03e6
joshuaboe <jboe@calpoly.edu>
2021-02-26 16:54:53 -0800

    dictionary key-values -> job-skills (#9)
    * changed FILENAME to FILEPATH
    
    * added comments
    
    * added appending to graphs
    
    * flipped skill-job pairs for job-skill pairs

--- src/graphCreatition.py
+++ src/graphCreatition.py
@@ -7,9 +7,8 @@ FILEPATH = 'test.edgelist'
 
 
 def main():
-
-    #G = generate_test_graph1()
-    G = read_graph(FILEPATH)
+    G = generate_test_graph1()
+    #G = read_graph(FILEPATH)
 
     # print(rank_jobs(G, ["Python", "JavaScript"]))
 
@@ -19,19 +18,37 @@ def main():
 
 
 # Create a test knowledge graph
+# keys = "[job title] at [company name], [location]"
+# values = list of skills
 def generate_test_graph1():
     edge_dict = {}
-    edge_dict['JavaScript'] = ['Job1', 'Job2', 'Job3', 'Job4', 'Job5']
-    edge_dict['CSS'] = ['Job1', 'Job3', 'Job5']
-    edge_dict['Python'] = ['Job2', 'Job4']
+    edge_dict['Software Engineer II at Amazon, San Luis Obispo'] = [
+        'JavaScript', 'CSS'
+    ]
+    edge_dict['Job2'] = ['JavaScript', 'Python']
+    edge_dict['Job3'] = ['JavaScript', 'CSS']
+    edge_dict['Job4'] = ['JavaScript', 'Python']
+    edge_dict['Job5'] = ['JavaScript', 'CSS']
 
     return create_graph(edge_dict)
 
 
 # Create a Knowledge Graph, provided a dictionary of nodes and edges
-#   - e_dict: a dictionary of skill-job (key-value) pairs
+#   - e_dict: a dictionary of job-skills (key-value) pairs
 def create_graph(e_dict):
-    return nx.from_dict_of_lists(e_dict, create_using=nx.DiGraph())
+    return nx.from_dict_of_lists(e_dict, create_using=nx.DiGraph()).reverse()
+
+
+# Append new nodes and edges to a Knowledge Graph
+#   - G: a networkx DiGraph
+#   - e_dict: a dictionary of job-skills (key-value) pairs to be added
+def append_graph(G, e_dict):
+    G.add_nodes_from(e_dict)
+    node_edge_pairs = []
+    for skill, jobs in e_dict.items():
+        for job in jobs:
+            node_edge_pairs.append((skill, job))
+    G.add_edges_from(node_edge_pairs)
 
 
 # Append new nodes and edges to a Knowledge Graph
