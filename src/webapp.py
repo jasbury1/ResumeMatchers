e032863b95da32456b8b39c37c988fbb2bb25fb9
Joshua Boe <jboe@calpoly.edu>
2021-03-07 19:03:24 -0800

    added fetching jobs

--- src/webapp.py
+++ src/webapp.py
@@ -1,9 +1,15 @@
 import os
 import shutil
 from resumeParse import get_skills_from_resume
-from flask import Flask, flash, request, redirect, render_template, url_for
+from graphCreatition import read_graph
+from jobParse import format_job_strings
+from jobPredictor import rank_jobs
+from flask import Flask, flash, request, redirect, render_template, url_for, session
 
 app = Flask(__name__)
+app.secret_key = "cant guess this"
+
+KG_FILEPATH = "../knowledgegraph.edgelist"
 
 # create new empty file path
 print("creating new path")
@@ -13,11 +19,13 @@ if os.path.exists(UPLOAD_PATH):
 os.makedirs(UPLOAD_PATH)
 app.config['UPLOAD_PATH'] = UPLOAD_PATH
 
+
 @app.route('/')
 def index():
     files = os.listdir(app.config['UPLOAD_PATH'])
     # render index template
     return render_template("index.html", files=files)
+
 
 @app.route('/', methods=["POST"])
 def uploadFile():
@@ -36,18 +44,37 @@ def uploadFile():
     # render index template
     return redirect(request.url)
 
+
 @app.route('/parse/<filename>')
 def getSkills(filename):
     skills = get_skills_from_resume(UPLOAD_PATH + filename)
+    session[
+        'skills'] = skills  # using a session variable to pass values between views
     return render_template("skills.html", skills=skills)
+
 
 @app.route('/parse/<filename>', methods=["POST"])
 def findJobs(filename):
     if request.form["submit_button"] == "Return":
         print("return")
         return redirect(url_for('index'))
+    elif request.form["submit_button"] == "Find Job Postings":
+        print("finding job postings")
+        return redirect(url_for('getJobs', filename=filename))
     else:
         return redirect(request.url)
+
+
+@app.route('/parse/<filename>/jobs', methods=["GET", "POST"])
+def getJobs(filename):
+    skills = session.get('skills', None)
+
+    kg = read_graph(KG_FILEPATH)
+    jobs = rank_jobs(kg, skills)
+    jobs = format_job_strings(jobs)
+
+    return render_template("jobs.html", jobs=jobs[:10])
+
 
 if __name__ == "__main__":
     app.run(debug=True)
